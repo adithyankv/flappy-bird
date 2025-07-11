fc0b0d5e60d3c3b5c577bac04ba7b3fa496d96ae
Adithyan K V <29067795+adithyankv@users.noreply.github.com>
2025-07-12 02:09:54 +0530

    Refactor and clean up scrolling code
    - Refactor infinite scroll into functions

--- main.py
+++ main.py
@@ -8,54 +8,45 @@ def main():
     SCREEN_WIDTH = 640
     SCREEN_HEIGHT = 480
 
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
 
     background = pygame.image.load("assets/images/background.png").convert()
     ground = pygame.image.load("assets/images/ground.png").convert()
-    # needs atleast two tiles to keep the screen always covered
-    num_background_tiles = max(2, math.ceil(SCREEN_WIDTH / background.get_width()))
-    num_ground_tiles = max(2, math.ceil(SCREEN_WIDTH / ground.get_width()))
 
     background_x = 0
     ground_x = 0
-
-    background_scroll_speed = 1
-    ground_scroll_speed = 4
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # if background_scroll < -background.get_width():
-        #     background_scroll = 0
+        draw_tiled_images(background, screen, background_x, 0)
+        draw_tiled_images(ground, screen, ground_x, SCREEN_HEIGHT - ground.get_height())
 
-        if background_x < -background.get_width():
-            background_x = 0
-
-        if ground_x < -ground.get_width():
-            ground_x = 0
-
-        for i in range(num_background_tiles):
-            screen.blit(background, (background_x + i * background.get_width(), 0))
-
-        for i in range(num_ground_tiles):
-            screen.blit(
-                ground,
-                (
-                    ground_x + i * ground.get_width(),
-                    SCREEN_HEIGHT - ground.get_height(),
-                ),
-            )
-        ground_x -= ground_scroll_speed
-        background_x -= background_scroll_speed
+        background_x = scroll_image(background, 1, background_x)
+        ground_x = scroll_image(ground, 4, ground_x)
 
         pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
+
+
+def draw_tiled_images(image, screen, x_pos, y_pos):
+    # Minimum number of tiles that are required to seamlessly cover whole screen
+    # Needs atleast two to always keep the screen covered
+    num_tiles = max(2, math.ceil(screen.get_width() / image.get_width()))
+    for i in range(num_tiles):
+        screen.blit(image, ((x_pos + i * image.get_width()), y_pos))
+
+
+def scroll_image(image, speed, x_pos):
+    if x_pos < -image.get_width():
+        return 0
+    return x_pos - speed
 
 
 if __name__ == "__main__":
